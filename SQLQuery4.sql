bd9d65dbf329c58d9bf9079dc78d5a92509b6e1a
Roman Puhachov <89234849+NewParadigmes@users.noreply.github.com>
2023-01-09 17:24:30 -0500

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,2 +1,17 @@
-Select  *
+Select Top 5 *
 From EmployeeDemographics
+Select only top 5 rows
+
+Select Distinct (emplyeeID)
+From EmployeeDemographics
+
+Select Count(LastName)
+From EmployeeDemographics
+
+shows all not Null values in column
+
+Select max(Salary) as MaxSalary
+       min(Salary) as MinSalary
+       Avg(Salary) as AvgSalary
+From EmplyeeSalary
+
